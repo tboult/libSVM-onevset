96cfbea9e96a9dc44c192071cdf6a59955586a3e
Terry Boult <tboult@vast.uccs.edu>
2013-07-09 22:11:52 -0600

    Cleanup after initial

--- onevtest/onevtest.sh
+++ onevtest/onevtest.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 	cd onevtest
-        for x in 0 1 2 3 ; do for y in 5 6 7 ; do  echo build iris-m-$x-$y;  ../svm-train -s $y -t $x iris.tr iris-m-$x-$y >> onevset.train; ../svm-predict  iris.t iris-m-$x-$y junk >> onevset.test  ;done ; done
+	echo "if all goes well you'll see 10 runs of Training then the phrase **Test Complete**"
+        rm -f onevset.train onevset.test
+        for x in 0 1 2 3 ; do for y in 5 6 7 ; do   ../svm-train -s $y -t $x iris.tr iris-m-$x-$y >> onevset.train; ../svm-predict  iris.t iris-m-$x-$y junk >> onevset.test  ;done ; done
 	diff -w onevset.train onevset.train.out
 	diff -w onevset.test onevset.test.out
-        echo "test complete"
+        rm -f iris-m* junk
+#        rm -f onevset.train onevset.test
+        echo "**Test Complete**"
+
 
